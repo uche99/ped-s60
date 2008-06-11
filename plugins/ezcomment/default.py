36007cbb287d73e634c3917dcde8c41b4e91ffb9
arkadiusz.wahlig <arkadiusz.wahlig@3505843e-634f-0410-9524-db06dcc73bd1>
2008-06-11 23:37:46 +0000

    Some code comments added.

--- plugins/ezcomment/default.py
+++ plugins/ezcomment/default.py
@@ -15,7 +15,7 @@ import os
 import ped
 import ui
 
-# our new ped.PythonFileWindow.control_key_press method
+# new ped.PythonFileWindow.control_key_press method
 def control_key_press(self, key):
     # check if the configured shortkey was pressed
     if key == ped.app.settings['ezcomment_key'].get():
@@ -51,20 +51,31 @@ def control_key_press(self, key):
         # let the old method control the key
         return old_control_key_press(self, key)
 
+# new ped.Application.apply_settings method
 def apply_settings(self):
+    # store the current language in oldlang
     oldlang = self.language
+    # call the old method, it will set the self.language if the user changed it
     old_apply_settings(self)
+    # if new language was selected, try to load it
     if self.language != oldlang:
-        _.load(os.path.join(path, 'lang\\%s' % self.language.encode('utf8')))
+        try:
+            translator.load(os.path.join(path, 'lang\\%s' % self.language.encode('utf8')))
+        except IOError:
+            # fall back to default (english)
+            translator.unload()
 
 if __name__ == '__main__':
     # plugin path
     path = os.path.split(sys.argv[0])[0]
 
     # i18n object
-    _ = ui.Translator()
+    translator = _ = ui.Translator()
     if ped.app.language != u'English':
-        _.load(os.path.join(path, 'lang\\%s' % ped.app.language.encode('utf8')))
+        try:
+            translator.load(os.path.join(path, 'lang\\%s' % ped.app.language.encode('utf8')))
+        except IOError:
+            pass
 
     # patch the ped.PythonFileWindow.control_key_press method
     old_control_key_press = ped.repattr(ped.PythonFileWindow,
